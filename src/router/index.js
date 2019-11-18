63ba6231809d975fee238eb785e4c783fe5682bf
kun <971909506@qq.com>
2019-11-18 16:31:05 +0800

    登录模块 -整合路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
 // 导入Vue
 import Vue from 'vue';
 // 导入路由
-import router from 'vue-router';
+// import router from 'vue-router';
 import VueRouter from 'vue-router';
 
 // 注册路由
 Vue.use(VueRouter);
 
+
+
+// 导入 login 组件
+import login from '../views/login/login.vue'
 // 创建路由规则
-const routes = [];
+const routes = [ // login
+    {path:'/login',component:login}];
 
 // 创建路由对象
 const router = new VueRouter({
-  routes
-});
+    routes
+  });
 
 // 暴露出去
 export default router;
