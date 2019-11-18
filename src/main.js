14f59a26af44acbc5f803f7273b465a3af6d5213
kun <971909506@qq.com>
2019-11-18 15:55:37 +0800

    整合路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+// 导入 Vue
 import Vue from 'vue'
+// 导入 根组件
 import App from './App.vue'
+// 导入 路由
+import router from './router'
 
+//是否显示生产提示
 Vue.config.productionTip = false
 
+// 创建Vue示例
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  // 挂载到Vue实例上
+  router
+})
+// 挂载到id为app的div上
+.$mount('#app')
